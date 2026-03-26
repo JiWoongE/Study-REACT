2a8243d6158c88856da305e58afe3cde33dad8e3
JiWoongE <147473223+JiWoongE@users.noreply.github.com>
2026-03-26 17:05:44 +0900

    useCallback

--- reactStudy/src/react_study/ex08_useCallback/ex02_debounce.jsx
+++ reactStudy/src/react_study/ex08_useCallback/ex02_debounce.jsx
@@ -1,21 +1,29 @@
-import { useState, useCallback } from "react";
 import debounce from "lodash.debounce";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 export default function SearchBox() {
   const [text, setText] = useState("");
-
-  const debouncedSearch = useCallback(
-    debounce((value) => {
-      console.log("API 요청:", value);
-    }, 500), // 0.5초 동안 대기 후 추가 입력이 없을 때 실행
-    []
-  );
-
+  // const debouncedSearch = useMemo(
+  //   ()=> {
+  //     return debounce((value)=> {
+  //     console.log("API 요청: ", value)
+  //   }, 500)
+  //   }
+  //   ,[]
+  // )
+  const debouncedSearch = debounce((value) => {
+    console.log("API 요청: ", value);
+  }, 500);
   const handleChange = (e) => {
     const value = e.target.value;
     setText(value);
     debouncedSearch(value);
   };
 
-  return <input value={text} onChange={handleChange} />;
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
+  return <input type="text" onChange={handleChange} />;
 }
